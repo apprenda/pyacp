88063ad4777cef6ba1f8eb5bb0b466e58090f373
Erik L <elustgarten@apprenda.com>
2017-03-27 16:32:06 -0400

    Adding functionality to return the first 1000 applications and custom properties, as well as the ability to transition a node

--- pyacp/apprenda_ops_client.py
+++ pyacp/apprenda_ops_client.py
@@ -1,5 +1,8 @@
 import requests
 import json
+
+import sys
+
 import pyacp.api_client
 from pyacp.apis.applications_api import ApplicationsApi
 from pyacp.apis.custom_properties_api import CustomPropertiesApi
@@ -29,21 +32,40 @@ class ApprendaOpsClient():
 
     def getApplications(self, alias = None):
         api = ApplicationsApi(self.internalClient)
+        kwargs = {'page_size':1000}
         if(alias is None):
-            return api.apps_search_new()
+            return api.apps_search_new(**kwargs).items
         else:
-            return api.api_v1_applications_app_alias_versions_get(alias)
+            return api.api_v1_applications_app_alias_versions_get(alias).items
 
     def getCustomProperties(self, id = None):
+        kwargs = {'page_size': 1000}
         api = CustomPropertiesApi(self.internalClient)
         if(id is None):
-            return api.custom_properties_get_public()
+            return api.custom_properties_get_public(**kwargs).items
         else:
             return api.custom_properties_get_single_public(id)
 
-    def getNodes(self, name = None):
+    def transitionNode(self, node, newstate):
+        if(node is None or newstate is None):
+            raise Exception('Please provide the name of the node to update and the new state')
         api = HostsApi(self.internalClient)
-        return api.api_v1_hosts_host_name_state_get(name)
+        response = api.api_v1_hosts_host_name_state_put(node, newstate)
+        if (response.status_code==204):
+            return True
+        elif (response.status_code==404):
+            raise Exception('The node provided was not found. Please ensure that the information passed is correct and try again.')
+        else:
+            raise Exception('Unknown error when setting the node state for: '+ node)
+
+
+    def getNodes(self, name = None):
+        kwargs = {'page_size': 1000}
+        api = HostsApi(self.internalClient)
+        if(name is None):
+            return
+        else:
+            return api.api_v1_hosts_host_name_state_get(name)
 
 
 
