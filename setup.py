9e3ad6bc5ef63f18fab80ad908be096f9a8dd9b2
Joseph Truncale <josephtruncale@gmail.com>
2017-04-25 11:32:39 -0400

    updated setup.py to create wheel and build script to rebuild wheel

--- setup.py
+++ setup.py
@@ -3,7 +3,21 @@
 """
     Platform Operations Rest API
 
-    The Platform Operations REST API can be leveraged to customize the Platform Operator experience of managing infrastructure and applications for the Apprenda Platform. This allows some of the functionality of the Platform's System Operations Center (SOC) to be accomplished through a variety of means such as custom-built UX or command-line interfaces.   For more information about the abilities of Platform Operators, see our documentation on the [SOC](/current/SOC).   ##Authentication   Before making a request, you must be authenticated. Learn how to [get authenticated](/restapi/platformops/v1/authentication).   ##Making a Request   ###Prerequisites   * Installed Platform of version 6.5.1 or later (Note that most endpoints are only available in verison 6.7.0 and later)    * Authentication token   * If SOC authorization is enabled on your Platform, you must be assigned as an active Platform Operator   ###Request URL   All requests must use **https**.   The URL for every request you make is the Cloud URI of your Platform followed by \"/soc\" and the path structure of the endpoint. For example, if your Cloud URI is apps.apprenda.harp and you want to get the Add-ons for your Platform, the request URI will be **apps.apprenda.harp/soc/api/v1/addons**.   For more information, see our documentation on [using API resources](/restapi/platformops/v1/using-resources) and [finding your Cloud URI](/current/clouduri).   ###Request Headers   Your authenication token must be passed in the header of all requests using the key **ApprendaSessionToken** (not case sensitive).  
+    The Platform Operations REST API can be leveraged to customize the Platform Operator experience of managing 
+    infrastructure and applications for the Apprenda Platform. This allows some of the functionality of the Platform's 
+    System Operations Center (SOC) to be accomplished through a variety of means such as custom-built UX or command-line
+     interfaces.   For more information about the abilities of Platform Operators, see our documentation on the 
+     [SOC](/current/SOC).   ##Authentication   Before making a request, you must be authenticated. Learn how to 
+     [get authenticated](/restapi/platformops/v1/authentication).   ##Making a Request   ###Prerequisites   
+     * Installed Platform of version 6.5.1 or later (Note that most endpoints are only available in verison 6.7.0 
+     and later)    * Authentication token   * If SOC authorization is enabled on your Platform, you must be assigned 
+     as an active Platform Operator   ###Request URL   All requests must use **https**.   The URL for every request 
+     you make is the Cloud URI of your Platform followed by \"/soc\" and the path structure of the endpoint. For e
+     xample, if your Cloud URI is apps.apprenda.harp and you want to get the Add-ons for your Platform, the request 
+     URI will be **apps.apprenda.harp/soc/api/v1/addons**.   For more information, see our documentation on 
+     [using API resources](/restapi/platformops/v1/using-resources) and [finding your Cloud URI](/current/clouduri).   
+     ###Request Headers   Your authenication token must be passed in the header of all requests using the key 
+     **ApprendaSessionToken** (not case sensitive).  
 
     OpenAPI spec version: v1
     
@@ -14,8 +28,8 @@
 import sys
 from setuptools import setup, find_packages
 
-NAME = "swagger_client"
-VERSION = "1.0.0"
+NAME = "pyacp"
+VERSION = "0.0.1"
 # To install the library, run the following
 #
 # python setup.py install
@@ -33,9 +47,24 @@ setup(
     url="",
     keywords=["Swagger", "Platform Operations Rest API"],
     install_requires=REQUIRES,
-    packages=find_packages(),
+    # packages=find_packages('pyacp'),
+    packages=['pyacp', 'pyacp/apis', 'pyacp/models', 'pyacp/services'],
     include_package_data=True,
     long_description="""\
-    The Platform Operations REST API can be leveraged to customize the Platform Operator experience of managing infrastructure and applications for the Apprenda Platform. This allows some of the functionality of the Platform&#39;s System Operations Center (SOC) to be accomplished through a variety of means such as custom-built UX or command-line interfaces.   For more information about the abilities of Platform Operators, see our documentation on the [SOC](/current/SOC).   ##Authentication   Before making a request, you must be authenticated. Learn how to [get authenticated](/restapi/platformops/v1/authentication).   ##Making a Request   ###Prerequisites   * Installed Platform of version 6.5.1 or later (Note that most endpoints are only available in verison 6.7.0 and later)    * Authentication token   * If SOC authorization is enabled on your Platform, you must be assigned as an active Platform Operator   ###Request URL   All requests must use **https**.   The URL for every request you make is the Cloud URI of your Platform followed by \&quot;/soc\&quot; and the path structure of the endpoint. For example, if your Cloud URI is apps.apprenda.harp and you want to get the Add-ons for your Platform, the request URI will be **apps.apprenda.harp/soc/api/v1/addons**.   For more information, see our documentation on [using API resources](/restapi/platformops/v1/using-resources) and [finding your Cloud URI](/current/clouduri).   ###Request Headers   Your authenication token must be passed in the header of all requests using the key **ApprendaSessionToken** (not case sensitive).  
+    The Platform Operations REST API can be leveraged to customize the Platform Operator experience of managing 
+    infrastructure and applications for the Apprenda Platform. This allows some of the functionality of the 
+    Platform&#39;s System Operations Center (SOC) to be accomplished through a variety of means such as custom-built UX 
+    or command-line interfaces.   For more information about the abilities of Platform Operators, see our documentation 
+    on the [SOC](/current/SOC).   ##Authentication   Before making a request, you must be authenticated. Learn how to 
+    [get authenticated](/restapi/platformops/v1/authentication).   ##Making a Request   ###Prerequisites   * Installed 
+    Platform of version 6.5.1 or later (Note that most endpoints are only available in verison 6.7.0 and later)    
+    * Authentication token   * If SOC authorization is enabled on your Platform, you must be assigned as an active 
+    Platform Operator   ###Request URL   All requests must use **https**.   The URL for every request you make is the 
+    Cloud URI of your Platform followed by \&quot;/soc\&quot; and the path structure of the endpoint. For example, if 
+    your Cloud URI is apps.apprenda.harp and you want to get the Add-ons for your Platform, the request URI will be 
+    **apps.apprenda.harp/soc/api/v1/addons**.   For more information, see our documentation on [using API resources]
+    (/restapi/platformops/v1/using-resources) and [finding your Cloud URI](/current/clouduri).   ###Request Headers   
+    Your authenication token must be passed in the header of all requests using the key **ApprendaSessionToken** 
+    (not case sensitive).  
     """
 )
